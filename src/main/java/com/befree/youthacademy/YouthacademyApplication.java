bcb658184a00ac6cf8c646f78ded40b54cea0ab1
Barry Hassane <hassane.barry@etu.univ-orleans.fr>
2024-02-21 20:05:39 +0100

    first commit

---
+++ src/main/java/com/befree/youthacademy/YouthacademyApplication.java
@@ -0,0 +1,13 @@
+package com.befree.youthacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthacademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouthacademyApplication.class, args);
+    }
+
+}
